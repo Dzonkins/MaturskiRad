0f451b398e7281b13678a16c2316e6584c7114c5
unknown <nikoladjordjevic.ns@gmail.com>
2025-02-14 15:47:15 +0100

    Prvi push

---
+++ ProgramZaRacunovodstvo/IzborFirme.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProgramZaRacunovodstvo
+{
+    /// <summary>
+    /// Interaction logic for GlavnaStrana.xaml
+    /// </summary>
+    public partial class IzborFirme : UserControl
+    {
+
+        private MainWindow _mainWindow;
+
+        public IzborFirme(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _mainWindow.ShowPrijava();
+        }
+    }
+}
---
+++ ProgramZaRacunovodstvo/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProgramZaRacunovodstvo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Prijava _prijava;
+        private IzborFirme _IzborFirme;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Prijava";
+            ShowOverlay(new Prijava(this));
+
+            _prijava = new Prijava(this);
+            _IzborFirme = new IzborFirme(this);
+        }
+
+        public void ShowPrijava()
+        {
+            ShowOverlay(_prijava);
+            this.Title = "Prijava";
+        }
+
+        public void ShowIzborFirme()
+        {
+            ShowOverlay(_IzborFirme);
+            this.Title = "Izbor Firme";
+        }
+
+        public void ShowOverlay(UserControl control)
+        {
+            OverlayContent.Content = control;
+            OverlayContainer.Visibility = Visibility.Visible;
+        }
+
+        public void HideOverlay()
+        {
+            OverlayContainer.Visibility = Visibility.Collapsed;
+            OverlayContent.Content = null;
+        }
+    }
+}
